25bf14698f403537e6bf74d5331bf99538f3380e
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 22:11:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,5 @@
 function forLoop(array){
+  var array= []
     for (let i = 0; i< 25; i++){
       if (i === 1) { array.push(`I am 1 strange loop.`)}
     else { array.push(`I am ${i} strange loops.`)}
