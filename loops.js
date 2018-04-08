0309f4420b6de74c1c3cb090fef5f00c4d670e80
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 21:20:51 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function makeArray(){
+  const array = []
+  const strangeArray =[]
+    for (let i = 0; i<25; i++){
+      if (i=1) { array.push("I am 1 strange loop.")}
+            return [array, i]}
+
+    const strangeArray = []
+
+    for (let i = 0; i < 25; i++) {
+      let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+}
