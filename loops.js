b219bb47271ec467bec7cc290cbdb8dbf2f53fc5
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 22:05:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function makeArray(){
   const array = []
-    for (let i = 0; i<25; i++){
+    for (let i = 0; i<array.length; i++){
       if (i === 1) { array.push(`I am 1 strange loop.`)}
     else { array.push(`I am ${i} strange loops.`)}
   return array
