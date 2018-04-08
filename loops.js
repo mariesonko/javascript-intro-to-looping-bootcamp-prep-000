03299f58ad308e7dd92abf3468340223d8c7c69c
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 22:27:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,8 +8,8 @@ function forLoop(array){
   }
   return array
 }
-function whileLoop (number){
-  let countdown = number
+function whileLoop (n){
+  let countdown = n
   while (countdown>0){
     console.log(---countdown)
   }
